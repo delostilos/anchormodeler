2710be7d10c9bc84868a4e657daa43b4a105738c
delostilos <delostilos@mac.com>
2016-05-13 23:16:23 +0200

    Created grails 3.1.6 angular project.

---
+++ grails-app/assets/javascripts/anchormodeler/anchormodeler.js
@@ -0,0 +1,9 @@
+//= wrapped
+//= require /angular/angular
+//= require /anchormodeler/core/anchormodeler.core
+//= require /anchormodeler/index/anchormodeler.index
+
+angular.module("anchormodeler", [
+    "anchormodeler.core",
+    "anchormodeler.index"
+]);
